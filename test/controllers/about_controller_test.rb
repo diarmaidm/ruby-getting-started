dbbab57dc29bf9f0166fe51fc88ec6bc09b46365
diarmaidm <diarmaidm@users.noreply.github.com>
2017-05-29 00:19:50 +0100

    add nav to layout and test for About

--- test/controllers/about_controller_test.rb
+++ test/controllers/about_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class AboutControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get the about page" do
+    get :about
+    assert_response :success
+    assert_select "h1", "About us."
+    assert true
+  end
 end
