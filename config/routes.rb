de04e5c2ed0d1144d40688bb7211d17e5d52ff17
diarmaidm <diarmaidm@users.noreply.github.com>
2017-05-28 17:51:57 +0100

    add calls to weather api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  get 'home/index'
+
   resources :widgets
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,7 +11,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'welcome#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
