a2f9355b32624e79fd5216f24b575398042bab00
diarmaidm <diarmaidm@users.noreply.github.com>
2017-05-29 00:01:24 +0100

    add some test cases

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,6 +4,9 @@ class HomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select "a", "PLAYON"
+    assert_select "button", "Submit"
+    assert_select 'title', "Ruby Getting Started on Heroku"
   end
 
 end
