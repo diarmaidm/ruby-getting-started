687e8b7ca62b3e997be280694e6c6943552139ec
diarmaidm <diarmaidm@users.noreply.github.com>
2017-05-31 00:37:49 +0100

    update home button

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,7 +4,7 @@ class HomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
-    # assert_select "a", "PLAYON"
+    # assert_select "a", "Home"
     # assert_select "button", "Submit"
     assert_select 'title', "Weather App for city"
   end
